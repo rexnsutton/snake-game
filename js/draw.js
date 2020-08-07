f10c590e390e84a28c8751f7d2c615c270af123f
Student Name <rexsutton2@gmail.cogit>
2020-08-07 11:18:07 -0500

    snake loader completed finished warmup for 08-07-2020

--- js/draw.js
+++ js/draw.js
@@ -1,9 +1,20 @@
 const canvas = document.querySelector(".canvas");
-const ctx = canvas.getContext("2d");
+const ctxt = canvas.getContext("2d");
 const scale = 10;
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
-var snake;
+let snake;
+(function setup() {
+
+ snake = new Snake();
+
+ window.setInterval(() => {
+        ctxt.clearRect(0, 0, canvas.width, canvas.height);
+        snake.update();
+        snake.draw();
+
+ }, 250);
+}());
 
 
 
